7c4b8fc1f19dca0a4a1899ec53daa15930591754
Bruce Huang <ck9312572005@gmail.com>
2017-04-21 01:07:34 +0800

    [Learning] Hello World function support
    1. add Hello World function and show strings by print

---
+++ define.h
@@ -0,0 +1,14 @@
+/* Variable type definition */
+#define UINT8   unsigned char
+#define INT8    signed char
+#define UCHAR   unsigned char
+#define CHAR    char
+#define UINT16  unsigned short int
+#define INT16   short int
+#define UINT32  unsigned int
+#define INT32   int
+#define UINT64  unsigned long long
+#define INT64   long long
+#define FLOAT   float
+#define DOUBLE  double
+#define BOOLEAN int
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "define.h"
+
+INT32 main()
+{
+	printf("Hello world!!\n");
+	return 0;
+}
